2c9095a05491196665161e502128857ad55a4b0f
RodriArgCba <44383090+RodriArgCba@users.noreply.github.com>
2020-12-02 01:31:33 -0300

    Commit inicial, creando metodos para facilitar creación de pantallas

---
+++ main.py
@@ -0,0 +1,15 @@
+import kivy.app
+kivy.require('1.11.1')
+from view.MenuPrincipal import MenuPrincipal
+
+
+
+from kivy.app import App
+
+
+class EntrenadorCulturalApp(App):
+    def build(self):
+        return MenuPrincipal()
+
+if __name__ == '__main__':
+    EntrenadorCulturalApp().run()
